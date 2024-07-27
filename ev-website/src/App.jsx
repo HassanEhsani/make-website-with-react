120806a84f1c080710a40fefeab6a3245a270f87
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-07-27 16:14:41 +0300

    start

--- ev-website/src/App.jsx
+++ ev-website/src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
+import Background from "./Components/Background/Background"
 
 const App = () => {
+  let heroData = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give in to",text2:"your passions"},
+  ]
+  const [heroCount,setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
   return (
     <div>
-      
+      <Background playStatus={playStatus} heroCount={heroCount}/>
     </div>
   )
 }
